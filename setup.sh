d8b9f0a6fdf9102e877c76baa2000d1d9a04e54b
Henzzzzzy <emmyhenz97@gmail.com>
2025-02-20 01:20:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
+---
+
+## **2. setup.sh**
+
+```bash
 #!/bin/bash
-echo "Installing REAL HACKING TOOLS..."
-pkg install python python2 php curl openssh wget git -y
+# setup.sh – Install dependencies and clone external tools
+
+echo -e "\033[1;32m[+] Installing required Termux packages...\033[0m"
+pkg update && pkg upgrade -y
+pkg install git python python2 curl wget php openssh -y
 pip install requests bs4 mechanize
 
-echo "Downloading hacking tools..."
-git clone https://github.com/htr-tech/zphisher     # Phishing Toolkit
-git clone https://github.com/thelinuxchoice/saycheese # Camera Hijack
-git clone https://github.com/sqlmapproject/sqlmap  # SQL Injection
-git clone https://github.com/Gameye98/Lazymux     # Multiple hacking tools
-git clone https://github.com/vanhauser-thc/thc-hydra # Brute-force Attacks
-git clone https://github.com/KasRoudra/TBomb      # SMS & Call Spammer
-git clone https://github.com/entynetproject/ghost # Payload Generator
+echo -e "\033[1;32m[+] Cloning external hacking tools (if required)...\033[0m"
+# Example clones – adjust or remove if you want to include your own tool implementations
+git clone https://github.com/htr-tech/zphisher.git || true
+git clone https://github.com/Techzindia/venom.git || true
 
-echo "All tools installed! Run './henz-hack.sh' to start hacking!"
+echo -e "\033[1;32m[+] Setup complete! Run 'bash henz-hack.sh' to start the toolkit.\033[0m"
